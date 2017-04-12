3e4a99a3218e46eae69601c0e45fad291a36da01
private <user@example.com>
2017-04-12 22:37:50 +0100

    Add support to create new books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,25 @@
 class BooksController < ApplicationController
   def index
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to root_path, notice: "Successfully created book #{@book.title}."
+    else
+      flash.now[:alert] = @book.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :filename)
+  end
 end
