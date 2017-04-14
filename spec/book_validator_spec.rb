c0bb72c01ab44f5a522310cf7de07f0f81832f6a
private <user@example.com>
2017-04-15 00:19:17 +0100

    Add validation results to the book validator

--- spec/book_validator_spec.rb
+++ spec/book_validator_spec.rb
@@ -2,14 +2,37 @@ require "rails_helper"
 require "book_validator"
 
 RSpec.describe BookValidator do
-  let(:valid_book)   {Book.new(title: "the title", filename: "filename.pdf")}
-  let(:invalid_book) {Book.new}
-
-  it "validates a book" do
-    expect(BookValidator.valid?(valid_book)).to be_truthy
+  def validation_of(book)
+    BookValidator.validate(book)
   end
 
-  it "does not validate books with missing data" do
-    expect(BookValidator.valid?(invalid_book)).to be_falsy
+  let(:invalid_book) {Book.new}
+
+  context "valid book" do
+    let(:valid_book) { Book.new(title: "the title", filename: "filename.pdf") }
+
+    it "is valid" do
+      expect(validation_of(valid_book)).to be_okay
+    end
+
+    it "does not return any messages" do
+      expect(validation_of(valid_book).messages).to be_empty
+    end
+  end
+
+  context "invalid book" do
+    let(:invalid_book) {Book.new}
+
+    it "is not valid" do
+      expect(validation_of(invalid_book)).not_to be_okay
+    end
+
+    it "returns validation messages" do
+      expect(validation_of(invalid_book).messages).not_to be_empty
+    end
+
+    it "validates the title" do
+      expect(validation_of(invalid_book).messages).to include("Title can't be empty.")
+    end
   end
 end
